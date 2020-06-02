2e2d5636fa639b4ddbf2e72565a6d1851aecad55
mivafox <mivafox@yandex.ru>
2020-06-02 20:06:11 +0300

    Выполненная 7-ая домашняя работа

---
+++ src/com/company/mivafox/Plate.java
@@ -0,0 +1,26 @@
+package com.company.mivafox;
+
+public class Plate
+{
+    private int food;
+
+    public Plate(int food)
+    {
+        this.food = food;
+    }
+
+    public void increaseFood(int food)
+    {
+        this.food += food;
+    }
+
+    public void decreaseFood(int appetite)
+    {
+        food = food - appetite;
+    }
+
+    public boolean hasEnoughFood(int appetite)
+    {
+        return appetite <= food;
+    }
+}
